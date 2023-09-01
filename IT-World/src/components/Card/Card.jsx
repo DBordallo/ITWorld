bd12e39dfc912652500005431cd73b3b24973d9c
Diego Bordallo <bordallo.d@gmail.com>
2023-09-01 15:00:18 +0200

    create css

--- IT-World/src/components/Card/Card.jsx
+++ IT-World/src/components/Card/Card.jsx
@@ -1,20 +1,23 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import './Card.css'
+import '../assets/img-cards/Portatil.jpg'
 
 function Cards() {
-  return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
-  );
-}
-
-export default Cards;
+    return (
+      <Card className="card" style={{ width: '18rem' }}>
+        <Card.Img className="card-img" variant="top" src="." />
+        <Card.Body>
+          <Card.Title className="card-title">Ordenador Portatil</Card.Title>
+          <Card.Text className="card-text">
+            Breve descripción del producto
+          </Card.Text>
+          <Button className="button-primary" variant="primary">
+            Entrar
+          </Button>
+        </Card.Body>
+      </Card>
+    );
+  }
+  
+  export default Cards;
--- IT-World/src/main.jsx
+++ IT-World/src/main.jsx
@@ -3,16 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import Cards from './components/Card/Card.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <Cards />
-    <Cards />
-    <Cards />
-    <Cards />
-    <Cards />
-    <Cards />
     <Cards />
   </React.StrictMode>,
 )
