800f13a5036385889c09026ed96a8e0c2424d57c
Diego Bordallo <bordallo.d@gmail.com>
2023-09-06 18:58:39 +0200

    add Carousel

--- IT-World/src/main.jsx
+++ IT-World/src/main.jsx
@@ -4,10 +4,12 @@ import App from './App.jsx'
 import './index.css'
 import Cards from './components/Card/Card.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Carousels from './components/Carousel/Carousel.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Carousels />
     <Cards />
   </React.StrictMode>,
 )
